923073454ebe3a5e93159459f8802a018b46cd44
billsioros <billsioros97@gmail.com>
2018-09-23 16:55:44 +0300

    vec2 up

--- src/vector2.cpp
+++ src/vector2.cpp
@@ -9,11 +9,15 @@ std::ostream& operator<<(std::ostream& out, const Vector2& vec2)
     out << std::fixed << std::setprecision(5) << std::showpos << vec2._x;
     out << ", ";
     out << std::fixed << std::setprecision(5) << std::showpos << vec2._y;
+
+    return out;
 }
 
 std::istream& operator>>(std::istream& in, Vector2& vec2)
 {
     in >> vec2._x >> vec2._y;
+
+    return in;
 }
 
 // Operations:
@@ -94,23 +98,23 @@ Vector2& Vector2::operator/=(double lambda)
     return *this;
 }
 
-inline bool operator< (const Vector2& A, const Vector2& B)
+bool operator< (const Vector2& A, const Vector2& B)
 {
     return (A._x < B._x) ||
            (A._x == B._x && A._y < B._y);
 }
 
-inline bool operator> (const Vector2& A, const Vector2& B)
+bool operator> (const Vector2& A, const Vector2& B)
 {
     return B < A;
 }
 
-inline bool operator==(const Vector2& A, const Vector2& B)
+bool operator==(const Vector2& A, const Vector2& B)
 {
     return !(A < B) && !(B < A);
 }
 
-inline bool operator!=(const Vector2& A, const Vector2& B)
+bool operator!=(const Vector2& A, const Vector2& B)
 {
     return !(A == B);
 }
